5eb08d8064c600e14e0949706ad9bcf28dd25905
Towhid47 <mdtowhidurrahman47@gmail.com>
2024-01-24 09:35:46 +0600

    custom JSON data folder added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,9 +6,17 @@ const Banner = () => {
         <div className='banner-image h-96'>
            
              <div className='flex items-center h-full justify-center flex-col banner-text text-white'>
-                  <h1 className='text-5xl'>Find Your Perfect Dream Job</h1><br />
+                  <h1 className='md:text-5xl  text-2xl'>Find Your Perfect Dream Job</h1><br />
                   <p className='w-52 md:w-full'>Job Finder lets you find out your preferred job very easily  </p>
              </div>
+
+
+            
+             
+
+                   
+
+             
  
         </div>
     );
