a9dbab41ce606e1405a533341f7dd9e69cf22d05
Towhid47 <mdtowhidurrahman47@gmail.com>
2024-01-24 10:27:52 +0600

    Banner search bar design done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Banner.css';
+import { FaSearch } from "react-icons/fa";
 
 const Banner = () => {
     return (
@@ -8,16 +9,16 @@ const Banner = () => {
              <div className='flex items-center h-full justify-center flex-col banner-text text-white'>
                   <h1 className='md:text-5xl  text-2xl'>Find Your Perfect Dream Job</h1><br />
                   <p className='w-52 md:w-full'>Job Finder lets you find out your preferred job very easily  </p>
+                  <br /><br />
+                 
+                  {/* ///////// Input Search Bar /////////////// */}
+
+                  <div className='p-3 searchBar-container lg:w-4/12 flex justify-between items-center'>
+                        <FaSearch className='inline text-xl me-5'/> 
+                        <input type="text" placeholder='Search by Category' className='w-full placeholder:text-slate-300'/>
+                  </div>
              </div>
-
-
-            
-             
-
                    
-
-             
- 
         </div>
     );
 };
