d7d87fe1cdea15d0dac4261d3bb184fc7a33d27e
Towhid47 <mdtowhidurrahman47@gmail.com>
2024-01-30 02:12:18 +0600

    AboutUs page have been modified for mobile responsive

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
 
     return (
         <div className='about-bg flex justify-center items-center'>
-            <div className='border-2 border-white w-1/2 text-white p-7'>
+            <div className='border-2 border-white w-11/12 md:w-1/2 text-white p-7'>
                 <h1 className='text-5xl text-center font-semibold'>About Us</h1><br />
                 <p className='text-xl'>Job Finder is the growing career management site in the country. Eight young business and IT professional backed by strong command over e-business and in-depth understanding of the needs of job seekers and employers in the country's context started this venture on July 2000.</p>
 
